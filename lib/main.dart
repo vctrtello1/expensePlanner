75435d15396602ef31c16723e21ebc8d201d7a59
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-03-07 13:02:28 -0600

    list of cards

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
+// ignore: prefer_const_constructors
 void main() => (runApp(MyApp()));
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Flutter App',
       home: MyHomePage(),
     );
   }
 }
 
+// ignore: use_key_in_widget_constructors
 class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
+  final List<Transaction> transactions = [
+    Transaction(id: 1, title: "Mouse", amount: 300, date: DateTime.now()),
+    Transaction(id: 2, title: "Food", amount: 700, date: DateTime.now()),
+    Transaction(id: 3, title: "Dinner", amount: 400, date: DateTime.now()),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +29,26 @@ class MyHomePage extends StatelessWidget {
         title: const Text('Flutter App'),
       ),
       // ignore: prefer_const_constructors
-      body: Center(
-        child: const Text('Widget playground'),
+      body: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        // ignore: prefer_const_literals_to_create_immutables
+        children: <Widget>[
+          const SizedBox(
+            width: double.infinity,
+            child: Card(
+              elevation: 5,
+              child: Text('Chart'),
+            ),
+          ),
+          Column(
+              children: transactions.map((tx) {
+            return Card(
+              child: Text(tx.title),
+            );
+          }).toList())
+        ],
       ),
     );
   }
