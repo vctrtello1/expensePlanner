161c26952c8a3831eeb47f476436eeb4dc34a291
Victor Hugo Tello <victorhugotello@hotmail.com>
2023-03-13 20:37:40 -0600

    add image as background

--- lib/main.dart
+++ lib/main.dart
@@ -25,11 +25,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final List<Transaction> _userTransaction = [
-    Transaction(id: 1, title: "Mouse", amount: 300, date: DateTime.now()),
-    Transaction(id: 2, title: "Food", amount: 700, date: DateTime.now()),
-    Transaction(id: 3, title: "Dinner", amount: 400, date: DateTime.now()),
-  ];
+  final List<Transaction> _userTransaction = [];
 
   void _addNewTransaction(String title, double amount) {
     final newTx =
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,40 +13,53 @@ class TransactionList extends StatelessWidget {
     return SizedBox(
         height: 300,
         // solucion para listas grandes con mejor performance
-        child: ListView.builder(
-          itemBuilder: (ctx, index) {
-            return Card(
-              child: Row(
+        child: transactions.isEmpty
+            ? Column(
                 children: <Widget>[
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                        border: Border.all(color: Colors.black, width: 2)),
-                    padding: const EdgeInsets.all(10),
-                    child: Text(
-                      '\$${transactions[index].amount.toStringAsFixed(2)}',
-                      style: const TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        transactions[index].title,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 15),
-                      ),
-                      Text(DateFormat('MM-dd-yyyy')
-                          .format(transactions[index].date))
-                    ],
-                  )
+                  const Text('No transactions added yet'),
+                  SizedBox(
+                      height: 200,
+                      child: Image.asset(
+                        'assets/images/waiting.png',
+                        fit: BoxFit.cover,
+                      ))
                 ],
-              ),
-            );
-          },
-          itemCount: transactions.length,
-        ));
+              )
+            : ListView.builder(
+                itemBuilder: (ctx, index) {
+                  return Card(
+                    child: Row(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 10, horizontal: 15),
+                          decoration: BoxDecoration(
+                              border:
+                                  Border.all(color: Colors.black, width: 2)),
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            '\$${transactions[index].amount.toStringAsFixed(2)}',
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 20),
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              transactions[index].title,
+                              style: const TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 15),
+                            ),
+                            Text(DateFormat('MM-dd-yyyy')
+                                .format(transactions[index].date))
+                          ],
+                        )
+                      ],
+                    ),
+                  );
+                },
+                itemCount: transactions.length,
+              ));
   }
 }
